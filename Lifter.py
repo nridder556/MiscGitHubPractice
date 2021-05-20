6a79fcc6cd03b24724aff03a472b038e415ddb24
nridder <n_ridder@comcast.net>
2021-05-19 22:04:21 -0400

    nridder-master-addWilks-051921

--- Lifter.py
+++ Lifter.py
@@ -22,11 +22,27 @@ class Lifter:
         bmi = self.weight/math.pow(self.height, 2)*703
         return bmi
 
+
+    def getWilks(self):
+        a =-216.0475144
+        b = 16.2606339
+        c = -0.002388645
+        d = -0.00113732
+        e = 7.01863E-06
+        f = -1.291E-08
+
+        return (self.getTotal()*500)/(a + b * self.weight + c*math.pow(self.weight, 2)
+                                      + d * math.pow(self.weight, 3)+e*math.pow(self.weight, 4) +
+                                      f * math.pow(self.weight, 5))
+
 class OlympicLifter(Lifter):
     def __init__(self,fn,ln,wt,ht,sntch,cnj):
         super().__init__(self,fn,ln,wt,ht,None,None)
         self.snatch = sntch
         self.cj = cnj
+
+
+
 
 
 
